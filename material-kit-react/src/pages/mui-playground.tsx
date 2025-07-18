b4411a29a09aa1767e84cde87b0541e6a46146e7
Meirunas Smitas <45215244+Meirunas@users.noreply.github.com>
2025-07-18 14:09:03 +0100

    move card in top middle

--- material-kit-react/src/pages/mui-playground.tsx
+++ material-kit-react/src/pages/mui-playground.tsx
@@ -7,32 +7,46 @@ import Typography from '@mui/material/Typography';
 
 export default function MUIPlayground() {
   return (
-    <Card variant="outlined" sx={{ maxWidth: 360 }}>
-      <Box sx={{ p: 2 }}>
-        <Stack direction="row" sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography gutterBottom variant="h5" component="div">
-            Toothbrush
+    <Box
+      sx={{
+        minHeight: '100vh',            
+        display: 'flex',               
+        alignItems: 'flex-start',          
+        justifyContent: 'center',      
+
+      }}
+    >
+      <Card variant="outlined" sx={{ maxWidth: 360, width: '100%' }}>
+        <Box sx={{ p: 2 }}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography gutterBottom variant="h5">
+              Toothbrush
+            </Typography>
+            <Typography gutterBottom variant="h6">
+              $4.50
+            </Typography>
+          </Stack>
+          <Typography variant="body2" color="text.secondary">
+            Pinstriped cornflower blue cotton blouse takes you on a walk to the park or just down
+            the hall.
           </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            $4.50
+        </Box>
+        <Divider />
+        <Box sx={{ p: 2 }}>
+          <Typography gutterBottom variant="body2">
+            Select type
           </Typography>
-        </Stack>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Pinstriped cornflower blue cotton blouse takes you on a walk to the park or just down the
-          hall.
-        </Typography>
-      </Box>
-      <Divider />
-      <Box sx={{ p: 2 }}>
-        <Typography gutterBottom variant="body2">
-          Select type
-        </Typography>
-        <Stack direction="row" spacing={1}>
-          <Chip color="primary" label="Soft" size="small" />
-          <Chip label="Medium" size="small" />
-          <Chip label="Hard" size="small" />
-        </Stack>
-      </Box>
-    </Card>
+          <Stack direction="row" spacing={1}>
+            <Chip color="primary" label="Soft" size="small" />
+            <Chip label="Medium" size="small" />
+            <Chip label="Hard" size="small" />
+          </Stack>
+        </Box>
+      </Card>
+    </Box>
   );
 }
